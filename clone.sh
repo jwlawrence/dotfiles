3fc0d03b5afc83a572d87fbb33a7822cb4a7c2d6
Joshua Lawrence <lawrencejw@gmail.com>
2021-06-14 14:48:33 -0700

    clone powerlevel10k on install

--- clone.sh
+++ clone.sh
@@ -4,5 +4,8 @@ echo "Cloning repositories..."
 
 DEVELOPMENT=$HOME/Development
 
+# System
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Personal
 # git clone git@github.com:jwlawrence/<REPO_NAME>.com.git $DEVELOPMENT/<REPO_NAME>.com
